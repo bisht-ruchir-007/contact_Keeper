e475e71a4e9c3a3ae80c6b317d4febfb8fefe744
Ruchir Bisht <ruchircr7@gmail.com>
2020-12-31 19:11:59 +0530

    Auth Middleware added to get user route

--- routes/auth.js
+++ routes/auth.js
@@ -5,13 +5,20 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const config = require('config');
+const auth = require('../middleware/auth');
 
 // @route  GET api/auth
 // @desc   Get logged in user
 // @access Private
-
-router.get('/', (req, res) => {
-	res.send('Get logged in user');
+// auth - middleware to validate the token
+router.get('/', auth, async (req, res) => {
+	try {
+		const user = await User.findById(req.user.id).select('-password');
+		res.json(user);
+	} catch (err) {
+		console.log(err.message);
+		res.status(500).send('Server Error');
+	}
 });
 
 // @route  POST api/auth
